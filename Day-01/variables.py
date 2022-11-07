fbbe5fc78e2096313d265b7dcc22c685b5211987
GIERCS <rravikumar34@gmail.com>
2022-11-07 17:08:37 +0530

    Day-01 Updated

---
+++ Day-01/variables.py
@@ -0,0 +1,58 @@
+##Variables:
+##      - To store a value
+##      - To Change a value
+##a = 10
+##print(a)
+##
+##b = 20
+##c = 30
+##print(b,c)
+##
+##d,t,y = 45,'Ramesh',78.0
+##print(d,t,y)
+##
+##q = w = e = 96
+##print(q,w,e)
+
+
+##Input:
+##      - By default it access string data
+##      - It displays an info to a user
+##      - It reads a data from User
+
+
+##j,k = input("Enter first value: "),input("Enter second value: ")
+##print(j,k)
+
+##
+##Type():
+##      - To know the data type of a value
+##      Syntax:
+##                  type(variablename)
+
+##j,k = input("Enter first value: "),input("Enter second value: ")
+##print(j,type(j))
+##print(k,type(k))
+
+##k = [12,45.056,False,'Tarun']
+##print(type(k))
+
+##
+##4
+##5
+##Addition of 4 and 5 is: ?
+
+##g = int(input("Enter a value: "))
+##h = int(input("Enter b value: "))
+##print("Addition of ",g,"and",h,"is: ",g+h)
+##print("Addition of %d and %d is: %d"%(g,h,g+h))
+##print("Addition of {} and {} is: {}".format(g,h,g+h))
+##print(f"Addition of {g} and {h} is: {g+h}")
+
+
+s = 1120
+b = 50
+print(s and b)
+print(s or b)
+print(s & b)
+print(s | b)
